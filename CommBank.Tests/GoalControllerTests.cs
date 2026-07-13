fe346db08b6c1c06b162f337708a748bc2ee660e
Arwin Dominic <arwindominic17@gmail.com>
2026-07-13 23:12:23 +0530

    Add test coverage for GetForUser route

--- CommBank.Tests/GoalControllerTests.cs
+++ CommBank.Tests/GoalControllerTests.cs
@@ -62,13 +62,28 @@ public class GoalControllerTests
         Assert.NotEqual(goals[1], result.Value);
     }
 
-    [Fact]
+[Fact]
     public async void GetForUser()
     {
         // Arrange
-        
+        var goals = collections.GetGoals();
+        var users = collections.GetUsers();
+        IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
+        IUsersService usersService = new FakeUsersService(users, users[0]);
+        GoalController controller = new(goalsService, usersService);
+
         // Act
-        
+        var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+        controller.ControllerContext.HttpContext = httpContext;
+        var result = await controller.GetForUser(goals[0].UserId!);
+
         // Assert
+        Assert.NotNull(result);
+
+        foreach (Goal goal in result!)
+        {
+            Assert.IsAssignableFrom<Goal>(goal);
+            Assert.Equal(goals[0].UserId, goal.UserId);
+        }
     }
-}
+}   // <-- add this closing brace for the class
